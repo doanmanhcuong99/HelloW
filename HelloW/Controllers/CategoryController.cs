35e4e0b7a5c420888a932e7c8c4be08211b74bc7
doanmanhcuong99 <you@example.com>
2018-11-15 08:28:54 +0700

    first commit

---
+++ HelloW/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloW.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelloW.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly MyDb _context;
+        public CategoryController(MyDb context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Detail(long id)
+        {
+            var category = _context.Categories
+                .Include(c => c.ListProducts)
+                .SingleOrDefault(c => c.Id == id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+    }
+}
---
+++ HelloW/Models/MyDb.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloW.Models
+{
+    public class MyDb : DbContext
+    {
+        public MyDb(DbContextOptions<MyDb> option)
+            : base(option)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>.HasData(
+                new Category()
+                {
+                    Id = 1,
+                    Name = "Đồ Nữ"
+                },
+                new Category()
+                {
+                    Id = 2,
+                    Name = "Đồ Nam"
+                });
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+    public class Category
+    {
+       [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public List<Product> ListProducts { get; set; }
+    }
+
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+
+        public long CategoryId { get; set; }
+        
+        public Category Category { get; set; }
+    }
+}
